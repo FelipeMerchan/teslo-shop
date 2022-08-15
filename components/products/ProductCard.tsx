670a6b84f65fd4eb1d8bbf1a3be0ecb89a1e8144
Felipe Merchan <felipe-sc@hotmail.es>
2022-08-15 16:17:51 -0500

    218. Mostrar textos cuando la imagen esta cargada

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -9,6 +9,8 @@ interface Props {
 
 export const ProductCard: FC<Props> = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
   const productImage = useMemo(() => {
     return isHovered
       ? `products/${product.images[1]}`
@@ -34,13 +36,14 @@ export const ProductCard: FC<Props> = ({ product }) => {
                 alt={product.title}
                 /* Cuando la imagen se carga podemos ejecutar acciones: */
                 /* onLoad={() => console.log('Cargo')} */
+                onLoad={() => setIsImageLoaded(true)}
               />
             </CardActionArea>
           </Link>
         </NextLink>
       </Card>
 
-      <Box sx={{ mt: 1 }} className='fadeIn'>
+      <Box sx={{ mt: 1, display: isImageLoaded ? 'block' : 'none' }} className='fadeIn'>
         <Typography fontWeight={700}>{product.title}</Typography>
         <Typography fontWeight={500}>${product.price}</Typography>
       </Box>
