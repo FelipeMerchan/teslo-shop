fe16a16aab44af36da922dd28ab19ac5be7e95f1
Felipe Merchan <felipe-sc@hotmail.es>
2022-08-14 21:30:48 -0500

    207. REST - Obtener producto por Slug

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { db } from '../../../database';
+import { IProduct } from '../../../interfaces';
+import { Product } from '../../../models';
+
+type Data =
+| { message: string }
+| IProduct;
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch (req.method) {
+        case 'GET':
+            return getProductBySlug(req, res);    
+        default:
+            return res.status(400).json({ message: 'Metodo no existe' })
+    }
+}
+
+const getProductBySlug = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const { slug } = req.query;
+
+    await db.connect();
+    const productInDB = await Product.findOne({ slug }).lean();
+    await db.disconnect();
+
+    if (!productInDB) {
+        return res.status(404).json({ message: `No hay un producto con el slug: ${slug}` })
+    }
+
+    return res.status(200).json(productInDB);
+}
