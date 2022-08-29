16f90de25a6252a0acdb4786f75ea14fea27d00f
Felipe Merchan <felipe-sc@hotmail.es>
2022-08-28 21:48:35 -0500

    229. Realizar la busqueda mediante SSR

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,14 +1,17 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetServerSideProps } from 'next';
 import { Typography } from '@mui/material';
 
-import { useProducts } from '../../hooks';
 import { ShopLayout } from '../../components/layouts';
 import { ProductList } from '../../components/products';
 import { FullScreenLoading } from '../../components/ui';
+import { dbProducts } from '../../database';
+import { IProduct } from '../../interfaces';
 
-const SearchPage: NextPage = () => {
-  const { productList, isLoading } = useProducts('/search/cybertruck');
+interface Props {
+  productList: IProduct[];
+}
 
+const SearchPage: NextPage<Props> = ({ productList }) => {
   return (
     <ShopLayout title='Teslo-Shop - Search' pageDescription='Encuentra los mejores productos de Teslo'>
       <Typography variant='h1' component='h1'>
@@ -17,14 +20,32 @@ const SearchPage: NextPage = () => {
       <Typography variant='h2' sx={{ mb: 1 }}>
         ABC - 123
       </Typography>
-
-      {
-        isLoading
-          ? <FullScreenLoading />
-          : <ProductList productList={productList} />
-      }
+  
+      <ProductList productList={productList} />
     </ShopLayout>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { query } = params as { query: string };
+
+  if(query.length ===0) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: true,
+      }
+    }
+  }
+
+  let productList = await dbProducts.getProductsByTerm(query);
+  // Todo: retornar otros productos cuando no hay productos con esa query
+
+  return {
+    props: {
+      productList,
+    }
+  }
+};
 
 export default SearchPage
