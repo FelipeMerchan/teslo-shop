506d1272e1407f7fa759329109df2035bfb662c1
Felipe Merchan <felipe-sc@hotmail.es>
2022-08-28 22:02:06 -0500

    230. Mejorar la experiencia de usuario en la busqueda

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,5 +1,5 @@
 import type { NextPage, GetServerSideProps } from 'next';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 import { ShopLayout } from '../../components/layouts';
 import { ProductList } from '../../components/products';
@@ -9,17 +9,26 @@ import { IProduct } from '../../interfaces';
 
 interface Props {
   productList: IProduct[];
+  foundProducts: boolean;
+  query: string;
 }
 
-const SearchPage: NextPage<Props> = ({ productList }) => {
+const SearchPage: NextPage<Props> = ({ productList, foundProducts, query }) => {
   return (
     <ShopLayout title='Teslo-Shop - Search' pageDescription='Encuentra los mejores productos de Teslo'>
       <Typography variant='h1' component='h1'>
-        Buscar producto
+        Buscar productos
       </Typography>
-      <Typography variant='h2' sx={{ mb: 1 }}>
-        ABC - 123
-      </Typography>
+      {
+        foundProducts
+          ? <Typography variant='h2' sx={{ mb: 1 }}>Término: {query}</Typography>
+          : (
+            <Box display='flex'>
+              <Typography variant='h2' sx={{ mb: 1 }}>No encontramos ningún producto</Typography>
+              <Typography variant='h2' sx={{ ml: 1 }} color='secondary'>{query}</Typography>
+            </Box>
+          )
+      }
   
       <ProductList productList={productList} />
     </ShopLayout>
@@ -29,7 +38,7 @@ const SearchPage: NextPage<Props> = ({ productList }) => {
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const { query } = params as { query: string };
 
-  if(query.length ===0) {
+  if(query.length === 0) {
     return {
       redirect: {
         destination: '/',
@@ -39,11 +48,14 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   }
 
   let productList = await dbProducts.getProductsByTerm(query);
+  const foundProducts = productList.length > 0;
   // Todo: retornar otros productos cuando no hay productos con esa query
 
   return {
     props: {
       productList,
+      foundProducts,
+      query,
     }
   }
 };
