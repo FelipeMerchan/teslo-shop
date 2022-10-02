f0f4085388c4edfe49ab74dc3a688c36e7a1683f
Felipe Merchan <felipe-sc@hotmail.es>
2022-10-01 22:15:15 -0500

    270. AuthContext

---
+++ context/auth/AuthProvider.tsx
@@ -0,0 +1,31 @@
+import { FC, useReducer } from 'react';
+import { IUser } from '../../interfaces';
+import { AuthContext, authReducer } from './';
+
+export interface AuthState {
+    isLoggedIn: boolean;
+    user?: IUser;
+}
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const AUTH_INITIAL_STATE: AuthState = {
+    isLoggedIn: false,
+    user: undefined,
+}
+
+export const AuthProvider: FC<Props> = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
+
+    return (
+        <AuthContext.Provider value={{
+            ...state,
+
+            // Methods
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
