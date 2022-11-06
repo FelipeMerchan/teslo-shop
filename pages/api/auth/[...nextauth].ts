ab984074e6abe7c31b4a94cbf88a0283743e9fab
Felipe Merchan <felipe-sc@hotmail.es>
2022-11-05 22:56:00 -0500

    299. NextAuth Callbacks

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -18,7 +18,7 @@ export const authOptions: NextAuthOptions = {
         //TODO: Validar contra base de datos
 
         /* En el caso de que la autenticacion falle debemos retornar null */
-        return { name: 'Felipe', correo: 'felipe@google.com', rol: 'admin' };
+        return { name: 'Felipe', correo: 'felipe@google.com', role: 'admin' };
       }
     }),
     GitHubProvider({
@@ -30,7 +30,36 @@ export const authOptions: NextAuthOptions = {
   //Callbacks
   /* Por defecto Neth Auth trabaja con JWT, pero se puede cambiar. */
   callbacks: {
-    
+    /* Primero se genera el JWT en el callback jwt y luego es pasado al
+    a la sesion con el callback session: */
+    async jwt({ token, account, user }) {
+      if (account) {
+        /* Asi podemos modificar el payload del JWT, agregando propiedades al token
+        que luego podemos encontrar en el JWT que se cree: */
+        token.accessToken = account.access_token;
+
+        switch (account.type) {
+          case 'oauth':
+            // TODO: crear usuario o verigicar si existe en mi DB
+            break;
+          case 'credentials':
+            token.user = user;
+            break;
+          default:
+            break;
+        }
+      }
+
+      return token;
+    },
+
+    /* Despues de generado el JWT en el callback jwt este se pasa a la sesion: */
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      session.user = token.user as any;
+
+      return session;
+    }
   },
 };
 
