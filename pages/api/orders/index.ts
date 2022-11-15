871b995634fe81ee7161540e3f1eeed0435f2244
Felipe Merchan <felipe-sc@hotmail.es>
2022-11-14 20:07:46 -0500

    317. Crear orden - Backend

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,4 +1,9 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+
+import { db } from '../../../database';
+import { IOrder } from '../../../interfaces';
+import { Product } from '../../../models';
 
 type Data = {
     message: string
@@ -14,7 +19,30 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 }
 
 const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-    const { body } = req;
+    const { orderItems, total } = req.body as IOrder;
+    // Verificar que el usuario este autenticado
+    const session = await getSession({ req });
+    if (!session) {
+        return res.status(401).json({ message: 'Debe estar autenticado para hacer esto'});
+    }
 
-    return res.status(201).json(body);
+    const productListIds = orderItems.map(product => product._id);
+    await db.connect();
+    const dbProductList = await Product.find({ _id: { $in: productListIds } });
+
+    try {
+        const subTotal = orderItems.reduce((prev, current) => {
+            const currentPrice = dbProductList.find(prod => prod._id === current._id)?.price;
+
+            if (!currentPrice) {
+                throw new Error('Verifique el carrito de nuevo. El producto no existe');
+            }
+
+            return (currentPrice * current.quantity) + prev
+        }, 0);
+    } catch (error) {
+
+    }
+
+    return res.status(201).json(req.body);
 };
