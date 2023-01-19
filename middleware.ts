426a81d502660c13afc3a52c2a13109fdce84d8a
Felipe Merchan <felipe-sc@hotmail.es>
2023-01-18 23:46:41 -0500

    353. Middleware para validar autenticacion

--- middleware.ts
+++ middleware.ts
@@ -17,6 +17,43 @@ export async function middleware(request: NextRequest) {
         return NextResponse.next();
     }
 
+    if (requestedPage.startsWith('/admin')) {
+        const session: any = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+
+        if (!session) {
+            const url = request.nextUrl.clone();
+            url.pathname = '/auth/login';
+            url.search = `?p=${requestedPage}`
+            return NextResponse.redirect(url);
+        }
+
+        const validRoles = ['admin', 'super-user', 'SEO'];
+
+        if (!validRoles.includes(session.user.role)) {
+            const url = request.nextUrl.clone();
+            url.pathname = '/'
+            return NextResponse.redirect(url);
+        }
+
+        return NextResponse.next();
+    }
+
+    if (requestedPage.startsWith('/api/admin/dashboard')) {
+        const session: any = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+
+        if (!session) {
+            return NextResponse.redirect(new URL('/api/auth/unautorized', request.url));
+        }
+
+        const validRoles = ['admin', 'super-user', 'SEO'];
+
+        if (!validRoles.includes(session.user.role)) {
+            return NextResponse.redirect(new URL('/api/auth/unautorized', request.url));
+        }
+
+        return NextResponse.next();
+    }
+
     /* // Version personalizada de la autenticacion:
     if (requestedPage.startsWith('/checkout/')) {
         const token = request.cookies.get('token');
@@ -33,5 +70,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: '/checkout/:path',
+    matcher: ['/checkout/:path', '/admin', '/api/admin/:path'],
 };
